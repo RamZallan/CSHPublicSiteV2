85356feb4f21952bc7123d9843a3fe3c4c9ff8a5
Ram Zallan <ramzallan@gmail.com>
2018-10-31 19:20:07 -0400

    Flesh out current public site's content

---
+++ assets/js/eboard.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+    if ($('#eboard').length) {
+        $('.eboard-photo').hover(function() {
+            console.log("AAAA");
+            $(this).fadeOut().attr('src', function(index, attr) {
+                return attr.replace('.jpg', '_joke.jpg');
+            }).fadeIn();
+        }, function() {
+            $(this).fadeOut().attr('src', function(index, attr) {
+                return attr.replace('_joke.jpg', '.jpg');
+            }).fadeIn();
+        });
+    }
+});
