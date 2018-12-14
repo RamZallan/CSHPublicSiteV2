abc48fa37ec18214cd47bc3f2f0eb5c66861dfcd
Ram Zallan <ramzallan@gmail.com>
2018-12-14 11:25:52 -0500

    Improve site responsiveness

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,6 +1,10 @@
 $(document).ready(() => {
     if ($('nav').length) {
         $(window).scroll(() => {
+            if ($(window).width() < 1000) { // don't condense nav on smaller screens
+                $('nav').first().removeClass('condensed');
+                return;
+            }
             let currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
             if (currentScroll > 150) {
                 $('nav').first().addClass('condensed');
